dbbb5775a2c0bb4b338cb61e1ae7d54eb7de8733
justinrpangestu <154042559+justinrpangestu@users.noreply.github.com>
2025-09-16 12:14:37 +0700

    update py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from fastapi import FastAPI
 from modules.items.routes import createItem, readItem, updateItem, deleteItem
+from typing import Union
 
 app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
 
 app.include_router(createItem.router)
 app.include_router(readItem.router)
