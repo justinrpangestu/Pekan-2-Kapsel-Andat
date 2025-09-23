ddf705ab816910e2486cfaf5c2888a3bf906c22a
justinrpangestu <154042559+justinrpangestu@users.noreply.github.com>
2025-09-23 12:18:06 +0700

    update Pekan 4

--- modules/items/schema/schemas.py
+++ modules/items/schema/schemas.py
@@ -1,8 +1,27 @@
 from typing import Optional
-from pydantic import BaseModel
+from pydantic import BaseModel,Field
 
 class Item(BaseModel):
     id: int
-    name: str
+    name: str= Field(
+    ...,
+    min_length=3,
+    max_length=10,
+    pattern=r'^[a-zA-Z0-9]+$',
+    title='Username',
+    description='must be alpha-numeric',
+    example='johndoe777'
+)
+
     description: Optional[str] = None
     price: float
+
+class ItemResponse(BaseModel):
+    id: int
+    name: str
+
+
+class ResponseModel(BaseModel):
+    success: bool
+    message: str
+    data: ItemResponse
